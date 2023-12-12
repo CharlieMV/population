7636798a7bd41e394ab82d6f2c7c5a9f49f84a03
CharlieMV <143232951+CharlieMV@users.noreply.github.com>
2023-12-12 09:59:35 -0800

    Add files via upload

--- Population.java
+++ Population.java
@@ -13,6 +13,9 @@ public class Population {
 	
 	// List of cities
 	private List<City> cities;
+	// Copy of list so that the original isn't affected
+	private ArrayList<City> newCities;
+	
 	// SortMethods object
 	private SortMethods sm;
 	
@@ -96,18 +99,162 @@ public class Population {
 		return -1;
 	}
 	
-	/** Find the 50 cities with the most population using selection sorting
+	/** Find the 50 cities with the least population using selection sort
 	 * 	and print them
 	 */
-	public void findMostPop() {
+	public void findLeastPop() {
 		// 	Copy list so old isn't affected
-		ArrayList<City> newCities = new ArrayList<City>(cities);
+		newCities = new ArrayList<City>(cities);
+		// 	Start timer
+		long startMillisec = System.currentTimeMillis();
+		//	Sort
 		newCities = sm.insertionSort(newCities);
-		for (int i = 0; i < 50; i++) {
-			System.out.println((i + 1) + ": " + newCities.get(i).getState());
-		}
-		System.out.println("\nElapsed Time");
+		// 	Stop timer
+		long endMillisec = System.currentTimeMillis();
+		//	Print list and time
+		System.out.println("Fifty least populous states");
+		printList(newCities);
+		System.out.println("\nElapsed Time " + 
+								(endMillisec - startMillisec) + "\n");
 	}
+	
+	/** Find the 50 cities with the most population using merge sort
+	 *	and print them
+	 */
+	public void findMostPop() {
+		// 	Copy list so old isn't affected.
+		newCities = new ArrayList<City>(cities);
+		// 	Start timer
+		long startMillisec = System.currentTimeMillis();
+		//	Sort
+		newCities = sm.mergeSort(newCities);
+		// 	Flip list
+		ArrayList<City> flippedCities = new ArrayList<City>();
+		for (int i = newCities.size() - 1; i >= 0; i--) {
+			flippedCities.add(newCities.get(i));
+		}
+		// 	Stop timer
+		long endMillisec = System.currentTimeMillis();
+		//	Print list and time
+		System.out.println("Fifty most populous states");
+		printList(flippedCities);
+		System.out.println("\nElapsed Time " + 
+								(endMillisec - startMillisec) + "\n");
+	}
+	
+	/**	Sort to find first 50 cities with asc name and print them
+	 */
+	public void findNameAsc() {
+		// 	Copy list so old isn't affected
+		newCities = new ArrayList<City>(cities);
+		// 	Start timer
+		long startMillisec = System.currentTimeMillis();
+		//	Sort
+		newCities = sm.insertionSortName(newCities);
+		// 	Stop timer
+		long endMillisec = System.currentTimeMillis();
+		//	Print list and time
+		System.out.println("Fifty cities sorted by name");
+		printList(newCities);
+		System.out.println("\nElapsed Time " + 
+								(endMillisec - startMillisec) + "\n");
+	}
+	
+	/** Sort to find first 50 cities with dec name and print them
+	 */
+	public void findNameDec() {
+		// 	Copy list so old isn't affected.
+		newCities = new ArrayList<City>(cities);
+		// 	Start timer
+		long startMillisec = System.currentTimeMillis();
+		//	Sort
+		newCities = sm.mergeSortName(newCities);
+		// 	Flip list
+		ArrayList<City> flippedCities = new ArrayList<City>();
+		for (int i = newCities.size() - 1; i >= 0; i--) {
+			flippedCities.add(newCities.get(i));
+		}
+		// 	Stop timer
+		long endMillisec = System.currentTimeMillis();
+		//	Print list and time
+		System.out.println("Fifty cities sorted by name descending");
+		printList(flippedCities);
+		System.out.println("\nElapsed Time " + 
+								(endMillisec - startMillisec) + "\n");
+	}
+	
+	/** Find 50 most populous cities in a state
+	 */
+	public void findPopState() {
+		Prompt in = new Prompt();
+		boolean isValid = false;
+		String state;
+		// 	Keep prompting user until a valid name is given
+		while (!isValid) {
+			state = in.getString("Enter state name (ie. Alabama)");
+			if (isValidState(state)) isValid = true;
+		}
+	}
+	
+	/** Check if the state is a valid state
+	 *	@param	String	String to check whether it is a state
+	 * 	@return	Boolean	Whether state is valid
+	 */
+	private boolean isValidState(String state) {
+		switch (state) {
+			case "Alabama":	case "Alaska":	case "Arizona":
+			case "Arkansas":	case "California":	case "Colorado":
+			case "Conneticut":	case "Delaware":	case "Florida":
+			case "Georgia":	case "Hawaii":	case "Idaho":
+			case "Illinois":	case "Indiana":	case "Iowa":
+			case "Kansas":	case "Kentuky":	case "Louisiana":
+			case "Maine": // Left off 12/12 in class
+		}
+	}
+	
+	/** Print first 50 elements of a list
+	 * 	@param	ArrayList<City>		list to print
+	 */
+	public void printList(ArrayList<City> arr) {
+		//	Size of the list should be size of the array, capped at 50
+		int listSize = arr.size();
+		if (listSize > 50) listSize = 50;
+		//	Header
+		System.out.printf("%9s", "State");
+		System.out.printf("%24s", "City");
+		System.out.printf("%30s", "Type");
+		System.out.printf("%31s", "Population");
+		System.out.println("");
+		//	List
+		for (int i = 0; i < listSize; i++) {
+			System.out.printf("%4s", (i + 1) + ": ");
+			System.out.printf("%-25s", arr.get(i).getState());
+			System.out.printf("%-30s", arr.get(i).getName());
+			System.out.printf("%-15s", arr.get(i).getDesignation());
+			System.out.printf("%20s", addCommas(arr.get(i).getPopulation()));
+			System.out.println("");
+		}
+	}
+	
+	/** Add commas to int as appropriate
+	 * 	@param	int		number to add commas to
+	 * 	@return String	number with commas
+	 */
+	private String addCommas(int num) {
+		// 	Set output as input
+		String output = Integer.toString(num);
+		// 	Keep track of num of digits since last comma
+		int numCount = 0;
+		for (int i = output.length() - 1; i > 0; i--) {
+			numCount++;
+			if (numCount == 3) {
+				output = output.substring(0, i) + "," + output.substring(i);
+				numCount = 0;
+			}
+		} 
+		return output;
+	}
+	
 	
 	public static void main(String[] args) {
 		// Initialize and instantiate 
@@ -124,13 +271,13 @@ public class Population {
 		boolean isClosed = false;
 		while(!isClosed) {
 			int selection = menuSelect();
-			System.out.println("\n");
+			System.out.println("");
 			switch (selection) {
-				case 1:	findMostPop(); break;
-				case 2:
-				case 3:
-				case 4:
-				case 5:
+				case 1:	findLeastPop(); break;
+				case 2: findMostPop();	break;
+				case 3:	findNameAsc();	break;
+				case 4:	findNameDec();	break;
+				case 5:	findPopState();	break;
 				case 6:
 				// Includes 9
 				default:	isClosed = true;;
--- SortMethods.java
+++ SortMethods.java
@@ -68,6 +68,23 @@ public class SortMethods {
 	}
 	
 	/**
+	 *	Insertion Sort algorithm - in ascending order, only comparing names
+	 *	@param arr		array of Integer objects to sort
+	 * 	@return ArrayList<City>		sorted array
+	 */
+	public ArrayList<City> insertionSortName(ArrayList<City> arr) {
+		for (int outer = 1; outer < arr.size(); outer ++) {
+			int inner = outer - 1;
+			while (inner >= 0 && arr.get(inner + 1).getName().compareTo(
+										arr.get(inner).getName()) < 0) {
+				swap(arr, inner, inner + 1);
+				inner --;
+			}
+		}
+		return arr;
+	}
+	
+	/**
 	 *	Merge Sort algorithm - in ascending order (you implement)
 	 *	@param arr		array of Integer objects to sort
 	 *  @return ArrayList<City>		sorted array
@@ -133,6 +150,94 @@ public class SortMethods {
 		while(!finished) {
 			// Add lowest to output
 			if (arr1.get(index1).compareTo(arr2.get(index2)) > 0) {
+				output.add(arr2.get(index2));
+				index2 ++;
+			} else {
+				output.add(arr1.get(index1));	
+				index1++;
+			}
+			/* If index reaches end of an array, copy the rest of the
+			 * other array to output */
+			if (index1 > arr1.size() - 1) {
+				while (index2 < arr2.size()) {
+					output.add(arr2.get(index2));
+					index2++;
+				}
+				finished = true;
+			}
+			if (index2 > arr2.size() - 1) {
+				while (index1 < arr1.size()) {
+					output.add(arr1.get(index1));
+					index1++;
+				}
+				finished = true;
+			}
+		}
+		return output;
+	}
+	
+	/**
+	 *	Merge Sort algorithm - in ascending order by only name
+	 *	@param arr		array of Integer objects to sort
+	 *  @return ArrayList<City>		sorted array
+	 */
+	public ArrayList<City> mergeSortName(ArrayList<City> arr) {
+		return (mergeSplitName(0, arr.size() - 1, arr));
+	}
+	
+	/**
+	 *  Split helper method for merge. If end - start indices = 1 or 2,
+	 * 	break the loop.
+	 * 	@param	int		start index
+	 * 	@param	int		end index
+	 * 	@param	ArrayList[]	array to split
+	 * 	@return ArrayList[]	merged array
+	 */
+	private ArrayList<City> mergeSplitName(int start, int end, ArrayList<City> arr) {
+		// Find middle to split
+		int splitIndex = (start + end) / 2;
+		// Array for each half
+		ArrayList<City> firstHalf = new ArrayList<City>();
+		ArrayList<City> secondHalf = new ArrayList<City>();
+		// First half
+		if (splitIndex - start > 1) {
+			firstHalf = mergeSplitName(start, splitIndex, arr);
+		} else if (splitIndex == start) {
+			firstHalf.add(arr.get(start));
+		} else {
+			// Swap if needed
+			if (arr.get(start).getName().compareTo(arr.get(start + 1)
+														.getName()) > 0)
+				swap(arr, start, start + 1);
+			firstHalf.add(arr.get(start));
+			firstHalf.add(arr.get(start + 1));
+		}
+		// Second half
+		if (end - (splitIndex + 1) > 1) {
+			secondHalf = mergeSplitName(splitIndex + 1, end, arr);
+		} else if (splitIndex + 1 == end) {
+			secondHalf.add(arr.get(end));
+		} else {
+			if (arr.get(splitIndex + 1).getName().compareTo(arr.get
+										(splitIndex + 2).getName()) > 0)
+				swap(arr, splitIndex + 1, splitIndex + 2);
+			secondHalf.add(arr.get(end - 1));
+			secondHalf.add(arr.get(end));
+		}
+		// Output Array
+		return mergeName(firstHalf, secondHalf);
+	}
+	private ArrayList<City> mergeName(ArrayList<City> arr1, ArrayList<City> arr2) {
+		// Array to output
+		ArrayList<City> output = new ArrayList<City>();
+		// Index of arrays
+		int index1 = 0;
+		int index2 = 0;
+		boolean finished = false;
+		while(!finished) {
+			// Add lowest to output
+			if (arr1.get(index1).getName().compareTo(arr2.get
+											(index2).getName()) > 0) {
 				output.add(arr2.get(index2));
 				index2 ++;
 			} else {
